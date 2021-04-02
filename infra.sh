c61f4a931fd115b33861c3f11b24ed6129cfa5e9
Victor Sanchez <victor@olympiu.com>
2021-04-01 23:05:35 -0500

    Added support for local registry.
    Signed-off-by: Victor Sanchez <victor@olympiu.com>

--- infra.sh
+++ infra.sh
@@ -1,14 +1,84 @@
 #!/bin/bash
+reg_name='kind-registry'
+reg_port='5000'
+reg_host="${reg_name}"
 
-function install() {
-    # install kind K8 cluster and add ingress controller
-    kind create cluster --config cluster-config.yml
+function local_registry() {
+    # create registry container unless it already exists
+
+    running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+    if [ "${running}" != 'true' ]; then
+    docker run \
+        -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+        registry:2
+    fi
+
+    reg_host="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' "${reg_name}")"
+    echo "Registry Host: ${reg_host}"
+}
+
+function post_local_registry() {
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
+}
+
+function create_cluster() {
+    # create a cluster with the local registry enabled in containerd
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_host}:5000"]
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+        authorization-mode: "AlwaysAllow"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+- role: worker
+- role: worker
+EOF
     kubectl cluster-info --context kind-kind
+}
+
+function basic() {
+    # create local registry
+    local_registry
+    create_cluster
+    post_local_registry
+
+    # install ingress controller
     kubectl create namespace ingress-nginx
     kubectl apply -n ingress-nginx -f base/ingress/mandatory.yaml
     kubectl apply -n ingress-nginx -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.27.0/deploy/static/provider/baremetal/service-nodeport.yaml
     kubectl patch deployments -n ingress-nginx nginx-ingress-controller -p '{"spec":{"template":{"spec":{"containers":[{"name":"nginx-ingress-controller","ports":[{"containerPort":80,"hostPort":80},{"containerPort":443,"hostPort":443}]}],"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}' 
-    
+}
+
+function install() {
+    # install basic kind cluster
+    basic
+
     # install psp
     kubectl apply -f base/policy/restricted-psp.yml
 
@@ -39,12 +109,16 @@ function install_keycloak() {
 }
 
 function delete () {
-    kind delete cluster
+    kind delete cluster || true
+    docker rm -f kind-registry || true
 }
 
 CMD=$1
 
 case $CMD in
+    basic)
+        basic
+        ;;
     install)
         install
         ;;
@@ -52,6 +126,6 @@ case $CMD in
         delete
         ;;
     *)
-        echo "Options are to install and delete only."
+        echo "Options are to basic, install and delete only."
         ;;
 esac
