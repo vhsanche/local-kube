356be9e706dfea97e6f190e7107b7d9cbbbf36e2
Victor Sanchez <victor.sanchez3@ngc.com>
2021-06-18 16:54:27 -0500

    Updated main.tf
    Signed-off-by: Victor Sanchez <victor.sanchez3@ngc.com>

--- main.tf
+++ main.tf
@@ -6,8 +6,16 @@ variable "registry1_password" {
   type = string
 }
 
+variable "reduce_flux_resources" {
+  type = bool
+  default = true
+}
+
+output "istio_gw_ip" {
+  value = module.big_bang.external_load_balancer.ip
+}
 module "big_bang" {
-  source = "git::https://repo1.dso.mil/platform-one/big-bang/terraform-modules/big-bang-terraform-launcher.git"
+  source = "git::https://repo1.dso.mil/platform-one/big-bang/terraform-modules/big-bang-terraform-launcher.git?ref=v0.1.2"
 
   big_bang_manifest_file = "bigbang/start.yaml"
   registry_credentials = [{
@@ -15,4 +23,5 @@ module "big_bang" {
     username = var.registry1_username
     password = var.registry1_password
   }]
+  reduce_flux_resources = var.reduce_flux_resources
 }
