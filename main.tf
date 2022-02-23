bb80416ed3e0801474ae417bcac6f2c362bbe99a
Victor Sanchez <vhsanche@gmail.com>
2022-02-22 22:01:40 -0600

    Enabled gitlab.
    Signed-off-by: Victor Sanchez <vhsanche@gmail.com>

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ variable "registry1_password" {
   type = string
 }
 
-variable "reduce_flux_resources" {
-  type = bool
-  default = true
-}
+# variable "reduce_flux_resources" {
+#   type = bool
+#   default = true
+# }
 
-output "istio_gw_ip" {
-  value = module.big_bang.external_load_balancer.ip
-}
+# output "istio_gw_ip" {
+#   value = module.big_bang.external_load_balancer.ip
+# }
 module "big_bang" {
 #  source = "git::https://repo1.dso.mil/platform-one/big-bang/terraform-modules/big-bang-terraform-launcher.git?ref=v0.1.2"
   source = "git::https://repo1.dso.mil/platform-one/big-bang/terraform-modules/big-bang-terraform-launcher.git"
@@ -24,5 +24,5 @@ module "big_bang" {
     username = var.registry1_username
     password = var.registry1_password
   }]
-  reduce_flux_resources = var.reduce_flux_resources
+  #reduce_flux_resources = var.reduce_flux_resources
 }
